c406182623cc0eda3ff2a5d07f075996be848275
Mingzhe Zou <zoumingzhe@qq.com>
2024-01-15 11:06:12 +0800

    version: 1.3

--- xpip/utils/attribute.py
+++ xpip/utils/attribute.py
@@ -3,7 +3,7 @@
 from urllib.parse import urljoin
 
 __name__ = "xpip-python"
-__version__ = "1.3.alpha.1"
+__version__ = "1.3"
 __description__ = "Python package. Build. Install."
 __url_home__ = "https://github.com/bondbox/xpip-python"
 __url_code__ = __url_home__
