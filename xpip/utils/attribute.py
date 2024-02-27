d83149b042533b2abc09f9df758a639e3a122acc
Mingzhe Zou <zoumingzhe@qq.com>
2024-02-27 17:17:56 +0800

    setup: attribute: fix __url_bugs__ error

--- xpip/utils/attribute.py
+++ xpip/utils/attribute.py
@@ -5,7 +5,7 @@ from urllib.parse import urljoin
 __name__ = "xpip-python"
 __version__ = "1.3"
 __description__ = "Python package. Build. Install."
-__url_home__ = "https://github.com/bondbox/xpip-python"
+__url_home__ = "https://github.com/bondbox/xpip-python/"
 __url_code__ = __url_home__
 __url_docs__ = __url_home__
 __url_bugs__ = urljoin(__url_home__, "issues")
