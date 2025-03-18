01665ead47e40741de2027b287ad8379c60a0b7e
Mingzhe Zou <zoumingzhe@qq.com>
2025-03-18 19:11:32 +0800

    xpip-build: bump version to 1.2.2

--- xpip_build/attribute.py
+++ xpip_build/attribute.py
@@ -3,7 +3,7 @@
 from urllib.parse import urljoin
 
 __project__ = "xpip.build"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __description__ = "Python package. Build. Install."
 __urlhome__ = "https://github.com/bondbox/xpip/"
 __urlcode__ = __urlhome__
